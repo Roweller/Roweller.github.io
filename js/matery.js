0e8f9f79112dd400965314735fb177788b1bc260
Roweller <2559617545@qq.com>
2022-11-04 23:27:46 +0800

    Site updated: 2022-11-04 23:27:45

--- js/matery.js
+++ js/matery.js
@@ -173,7 +173,7 @@ $(function () {
 
 //黑夜模式提醒开启功能
 setTimeout(function () {
-    if ((new Date().getHours() >= 19 || new Date().getHours() < 7) && !$('body').hasClass('DarkMode')) {
+    if ((new Date().getHours() >= 21 || new Date().getHours() < 7) && !$('body').hasClass('DarkMode')) {
         let toastHTML = '<span style="color:#97b8b2;border-radius: 10px;>' + '<i class="fa fa-bellaria-hidden="true"></i>晚上使用深色模式阅读更好哦。(ﾟ▽ﾟ)</span>'
         M.toast({ html: toastHTML })
     }
--- libs/others/popupText.js
+++ libs/others/popupText.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function ($) {
   $("body").click(function (e) {
     // 点击弹出文字数组
     var a = new Array
-      ("冲冲冲", "栓Q", "emo", "yyds", "淦", "锦鲤", "冲鸭", "Chur", "安排" ,"皮一下");
+      ("冲冲冲", "栓Q", "emo", "yyds", "淦", "锦鲤", "冲鸭", "欧克", "安排" ,"皮一下");
     var $i = $("<span/>").text(a[a_idx]);
     a_idx = (a_idx + 1) % a.length;
     var x = e.pageX,
